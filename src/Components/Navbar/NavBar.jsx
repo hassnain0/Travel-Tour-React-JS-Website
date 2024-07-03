f206adb00191f81d4a7144adc2cce0518e780c22
Hassnain Ali <113959400+hassnain0@users.noreply.github.com>
2024-07-03 23:58:18 +0500

    NavBar

---
+++ src/Components/Navbar/NavBar.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import "./NavBar.css";
+import { MdOutlineTravelExplore } from "react-icons/md";
+import { AiFillCloseCircle } from "react-icons/ai";
+import { TbGridDots } from "react-icons/tb";
+
+const NavBar = () => {
+  return (
+    <section className="navBarSection">
+      <header className="header flex">
+        <div className="logoDiv">
+          <a href="#" className="logo flex">
+            <h1>
+              <MdOutlineTravelExplore className="icon" /> Travel.
+            </h1>
+          </a>
+        </div>
+        <div className="navBar">
+          <ul className="navlists flex">
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Home
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Packages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Shop
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                About
+              </a>
+            </li>
+
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Pages
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                News
+              </a>
+            </li>
+            <li className="navItem">
+              <a href="#" className="navLink">
+                Contact
+              </a>
+            </li>
+            <button className="btn">
+            <a href="#">Book Now</a>
+            </button>
+          </ul>
+          <div className="closeNavbar"/>
+          <AiFillCloseCircle  className="icon"/>
+        </div>
+
+        <div className="toggleNavbar">
+        <TbGridDots  className="icon"/>
+        </div>
+      </header>
+    </section>
+  );
+};
+export default NavBar;
